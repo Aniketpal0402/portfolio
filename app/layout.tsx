c37c1aaf4f2e60518435092f8bce3cfe4ae4585a
Aniket Pal <aniket040205@gmail.com>
2025-11-03 00:51:57 +0530

    Responsive done

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       {/* app/layout.tsx */}
 <head>
   <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
   <link
     href="https://fonts.googleapis.com/css2?family=Staatliches&family=Stint+Ultra+Condensed&display=swap"
     rel="stylesheet"
